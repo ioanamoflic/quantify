aea334d8a21f0ac90631046062540c5dbdae458d
Ioana Moflic <30651672+ioanamoflic@users.noreply.github.com>
2022-02-21 23:20:27 +0200

    Edited optimizers

--- optimizers/cancel_ngh_cnots.py
+++ optimizers/cancel_ngh_cnots.py
@@ -1,18 +1,22 @@
 import cirq
-
+from optimization.optimize_circuits import CircuitIdentity
 from .transfer_flag_optimizer import TransferFlagOptimizer
 import quantify.utils.misc_utils as mu
 
 
 class CancelNghCNOTs(TransferFlagOptimizer):
     # Cancels two neighbouring CNOTs
-    def __init__(self, optimize_till: int = None):
+
+    def __init__(self, moment=None, qubit=None, only_count=False):
         super().__init__()
-        self.optimize_till = optimize_till
-        self.reward = 0.0
+        self.only_count = only_count
+        self.count = 0
+        self.moment_index_qubit = []
+        self.moment = moment
+        self.qubit = qubit
 
     def optimization_at(self, circuit, index, op):
-        if self.optimize_till is not None and index >= self.optimize_till:
+        if (index != self.moment or op.qubits[0] != self.qubit) and not self.only_count:
             return None
 
         if mu.my_isinstance(op, cirq.CNOT):
@@ -45,8 +49,7 @@ class CancelNghCNOTs(TransferFlagOptimizer):
             # print("next are ", next_op_h1, next_op_h2)
 
             # Are the operations Hadamards?
-            if mu.my_isinstance(next_op_cnot1, cirq.CNOT) \
-                and mu.my_isinstance(next_op_cnot2, cirq.CNOT) :
+            if mu.my_isinstance(next_op_cnot1, cirq.CNOT) and mu.my_isinstance(next_op_cnot2, cirq.CNOT):
 
                 # theoretically nxt_1 and nxt_2 should be equal
                 if nxt_1 != nxt_2:
@@ -62,10 +65,13 @@ class CancelNghCNOTs(TransferFlagOptimizer):
                 if self.transfer_flag:
                     mu.transfer_flags(circuit, op.qubits[0], index, nxt_1)
 
-                self.reward += 0.1
+                if self.only_count:
+                    self.count += 1
+                    self.moment_index_qubit.append((CircuitIdentity.CANCEL_CNOTS, index, op.qubits[0]))
+                    return None
 
                 return cirq.PointOptimizationSummary(
-                    clear_span = nxt_1 - index + 1,  # Range of moments to affect.
-                    clear_qubits = op.qubits,  # The set of qubits that should be cleared with each affected moment
-                    new_operations = [] # The operations to replace
+                    clear_span=nxt_1 - index + 1,  # Range of moments to affect.
+                    clear_qubits=op.qubits,  # The set of qubits that should be cleared with each affected moment
+                    new_operations=[]  # The operations to replace
                 )
--- optimizers/cancel_ngh_hadamard.py
+++ optimizers/cancel_ngh_hadamard.py
@@ -1,16 +1,22 @@
 import cirq
 
 import quantify.utils.misc_utils as mu
+from optimization.optimize_circuits import CircuitIdentity
 from .transfer_flag_optimizer import TransferFlagOptimizer
 
 
 class CancelNghHadamards(TransferFlagOptimizer):
-    def __init__(self, optimize_till: int = None):
+
+    def __init__(self, moment=None, qubit=None, only_count=False):
         super().__init__()
-        self.optimize_till = optimize_till
+        self.only_count = only_count
+        self.count = 0
+        self.moment_index_qubit = []
+        self.moment = moment
+        self.qubit = qubit
 
     def optimization_at(self, circuit, index, op):
-        if self.optimize_till is not None and index >= self.optimize_till:
+        if (index != self.moment or op.qubits[0] != self.qubit) and not self.only_count:
             return None
 
         if not (mu.my_isinstance(op, cirq.H)):
@@ -27,13 +33,17 @@ class CancelNghHadamards(TransferFlagOptimizer):
         next_op = circuit.operation_at(op.qubits[0], n_idx)
 
         if next_op.gate == cirq.H:
-
             if self.transfer_flag and (not mu.has_flag(next_op)):
                 # Optimize only flagged operations
                 return None
 
             if self.transfer_flag:
                 mu.transfer_flags(circuit, op.qubits[0], index, n_idx)
+
+            if self.only_count:
+                self.count += 1
+                self.moment_index_qubit.append((CircuitIdentity.CANCEL_HADAMARDS, index, op.qubits[0]))
+                return None
 
             # print('Hadamards cancelled ', index)
             return cirq.PointOptimizationSummary(clear_span=n_idx - index + 1,
